ffc45322c17e170491a3e3da403e301fc7e2f71d
ZiqingQiu <qziqing@gmail.com>
2017-10-24 13:48:57 -0400

    add book and bookrep class

---
+++ App_Code/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Book
+/// </summary>
+public class Book
+{
+    //PROPERTIES
+    public string BookName { get; set; }
+    public string AuthorName { get; set; }
+    public string ISBN { get; set; }
+
+
+    //CONSTUCTORS
+    public Book()
+    {
+    }
+
+    public Book(string bookname, string authorname, string isbn)
+    {
+        this.BookName = bookname;
+        this.AuthorName = authorname;
+        this.ISBN = isbn;
+    }
+}
---
+++ App_Code/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BookRepository
+/// </summary>
+public class BookRepository
+{
+    //FIELDS
+    private List<Book> _booklist;
+
+    //CONSTRUCTORS
+    public BookRepository()
+    {
+        _booklist = new List<Book>();
+    }
+
+    //PUBLIC METHODS
+    public List<Book> GetAllBooks()
+    {
+        return _booklist;
+    }
+
+    public void AddBook(Book b)
+    {
+        _booklist.Add(b);
+    }
+
+    public void DeleteBook(Book b)
+    {
+        _booklist.Remove(b);
+    }
+}
