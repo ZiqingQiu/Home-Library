97642c068430a7d379e79690ac064f0682f227bd
ZiqingQiu <qziqing@gmail.com>
2017-10-25 14:04:33 -0400

    add session based theme

--- Home.aspx.cs
+++ Home.aspx.cs
@@ -7,8 +7,16 @@ using System.Web.UI.WebControls;
 
 public partial class Home : System.Web.UI.Page
 {
+
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        if (Session["CurrrentTheme"] != null)
+        {
+            this.Theme = Session["CurrrentTheme"] as string;
+        }
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
     }
 }
---
+++ Setup.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Setup : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (null == Session["CurrrentTheme"])
+        {
+            Session["CurrrentTheme"] = "Light";
+        }
+
+
+    }
+
+    /// <summary>
+    /// store session based theme selection
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void ThemeChoice_Changed(object sender, EventArgs e)
+    {
+        if (rdoLight.Checked)
+        {
+            Session["CurrrentTheme"] = "Light";
+        }
+        if (rdoDark.Checked)
+        {
+            Session["CurrrentTheme"] = "Dark";
+        }
+    }
+}
